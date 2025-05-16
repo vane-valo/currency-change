1acfc6b644b778bb97f74ce89a9e5936d6897043
Vanessa Vaca <vanessa.vaca96@gmail.com>
2025-05-15 22:04:45 -0600

    create Currency.java

--- src/Currency.java
+++ src/Currency.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public record Currency() {
+public record Currency(String base_code,
+                       String target_code,
+                       double conversion_rate,
+                       double conversion_result,
+                       double amount) {
 }
