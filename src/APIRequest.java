6f187cef457f8f7795af70b54111fc740cc0e833
Vanessa Vaca <vanessa.vaca96@gmail.com>
2025-05-15 22:17:58 -0600

    edit

--- src/APIRequest.java
+++ src/APIRequest.java
@@ -1,6 +1,4 @@
 import com.google.gson.Gson;
-
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -9,8 +7,8 @@ import java.net.http.HttpResponse;
 public class APIRequest {
 
     public Currency currencyConversion(String base_code, String target_code, double amount){
-        String APIKey = "6eb8752d684bda000bfdce8a";
-        URI link = URI.create("https://v6.exchangerate-api.com/v6/" + APIKey + "/pair/" + base_code + "/" + target_code + "/"+amount);
+
+        URI link = URI.create("https://v6.exchangerate-api.com/v6/6eb8752d684bda000bfdce8a/pair/" + base_code + "/" + target_code + "/"+amount);
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
